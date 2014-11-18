53c93a35b1023f7af52a2b1167e7c7f4be11338b
Daichi Kimura <kimura@feedforce.jp>
2014-11-18 10:31:25 +0900

    Update Script
    - Frontmatterに執筆者`author`を追加。
    - ファイル名に記事スラッグを使用
    - Frontmatterの`title`に日本語タイトルを使用。

--- wordpress_to_middleman.rb
+++ wordpress_to_middleman.rb
@@ -12,7 +12,7 @@ require 'fileutils'
 # SETTINGS #
 WORDPRESS_XML_FILE_PATH = "#{ENV['PWD']}/wordpress.xml"  # THE LOCATION OF THE EXPORTED WORDPRESS ARCHIVE #
 OUTPUT_PATH = "#{ENV['PWD']}/export/_posts/"  # THE LOCATION OF THE SAVED POSTS #
-ORIGINAL_DOMAIN = "http://perpetuallybeta.com"  #  THE DOMAIN OF THE WEBSITE #
+ORIGINAL_DOMAIN = 'http://perpetuallybeta.com'  #  THE DOMAIN OF THE WEBSITE #
 SEPARATE_CATEGORIES_FROM_TAGS = false
 CONVERT_FROM_HTML = false
 
@@ -20,8 +20,8 @@ class Parser
 
 	def self.make_output_path
 		unless File.directory?(OUTPUT_PATH)
-    		FileUtils.mkdir_p(OUTPUT_PATH)
-    		puts "Saving all files in" + OUTPUT_PATH.to_s
+				FileUtils.mkdir_p(OUTPUT_PATH)
+				puts 'Saving all files in' + OUTPUT_PATH.to_s
 		end
 	end
 
@@ -29,39 +29,41 @@ class Parser
 		# xml = Nokogiri::HTML(open(WORDPRESS_XML_FILE_PATH.gsub("CDATA", "")))
 		f = File.open(WORDPRESS_XML_FILE_PATH)
 		xml = Nokogiri::XML(f)
-		posts = xml.css("item")
+		posts = xml.css('item')
 		
 		posts.each do |post|
 
 			# Parsing Post Frontmatter
 			# ------------------------------------
-			title = post.css("title").text
-			title.gsub!(":", "-")
-			post_date = post.xpath("wp:post_date").first.inner_text
+			title = post.xpath('wp:post_name').inner_text
+			title_ja = post.css('title').text
+			title.gsub!(':', '-')
+			post_date = post.xpath('wp:post_date').first.inner_text
 			created_at = Date.parse(post_date).to_s
+			author = post.at_xpath('.//dc:creator').inner_text
 
-			tags = ""
-			categories = ""
-			category_xml = post.xpath("category")
+			tags = ''
+			categories = ''
+			category_xml = post.xpath('category')
 
-			if SEPARATE_CATEGORIES_FROM_TAGS == true
+			if SEPARATE_CATEGORIES_FROM_TAGS
 				category_xml.each do |category|
-					if category.css("@domain").to_s == "category"
-						categories += category.css("@nicename").text + ", "
+					if category.css('@domain').to_s == 'category'
+						categories += category.css('@nicename').text + ', '
 					else
-						tags += category.css("@nicename").text + ", "
+						tags += category.css('@nicename').text + ', '
 					end
 				end
 			else
 				category_xml.each do |category|
-					tags += category.css("@nicename").text + ", "
+					tags += category.css('@nicename').text + ', '
 				end
 			end
 
 			# Parsing Post Content
 			# ------------------------------------
 			# content = post.at_xpath(".//content:encoded").to_s
-			content = post.at_xpath(".//content:encoded").inner_text
+			content = post.at_xpath('.//content:encoded').inner_text
 
 			# Cleaning up the HTML output of content
 			# content.gsub!("<encoded>", " ")
@@ -75,7 +77,7 @@ class Parser
 
 			# do a crude test for html tags
 			if CONVERT_FROM_HTML && /</ =~ content && />/ =~ content
-				content.gsub!(/(\r?\n\r?\n)/, "XXXXXXXXXX") # preserve double newline as token
+				content.gsub!(/(\r?\n\r?\n)/, 'XXXXXXXXXX') # preserve double newline as token
 				content.gsub!(/\n(<.+>)/, "<br><br>\\1") # add newline before html tags
 				md_content = Html2Md.new(content)
 				content = md_content.parse
@@ -85,22 +87,23 @@ class Parser
 			content.gsub! /\n\s*\n/, "\n\n" # collapse newlines
 
 			if !(created_at.nil? || title.nil? || post_date.nil? || content.nil?)
-				output_filename = OUTPUT_PATH + created_at + "-" + sanitize_filename(title) + ".markdown"
+				output_filename = OUTPUT_PATH + created_at + '-' + sanitize_filename(title) + '.md'
 				puts output_filename
 
-				file_content = "---" + "\n"
-				file_content += "title: " + title + "\n"
-				file_content += "date: " + post_date + "\n"
-				file_content += "tags: " + tags + "\n"
-				if SEPARATE_CATEGORIES_FROM_TAGS == true
-					file_content += "categories: " + categories + "\n" unless categories.empty?
+				file_content = '---' + "\n"
+				file_content += 'title: '  + title_ja + "\n"
+				file_content += 'date: '   + post_date + "\n"
+				file_content += 'author: ' + author + "\n"
+				file_content += 'tags: '   + tags + "\n"
+				if SEPARATE_CATEGORIES_FROM_TAGS
+					file_content += 'categories: ' + categories + '\n' unless categories.empty?
 				end
-				file_content += "---" + "\n"
+				file_content += '---' + "\n"
 				file_content += content
 
 				# Saving File
 				# ------------------------------------
-				File.open(output_filename, "w") do |f|     
+				File.open(output_filename, 'w') do |f|
 					f.write(file_content)
 				end
 			end
